cd6205448dfa3e28fbd67bb2fdde439eeecb928d
Angel <amoline@davisa.es>
2026-01-17 14:54:20 +0100

    sicronizar

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,8 +17,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False,
+    allow_origins=[
+        "https://react-router-git-main-angelote567s-projects.vercel.app",
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
